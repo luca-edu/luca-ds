f3041b0cdcdaf79ed89f11e25950d095a13666aa
edgar-ci <edgarlvzci@gmail.com>
2025-11-11 11:14:47 -0600

    chore: update .gitignore, modify build output path for Storybook, and add GitHub Actions workflow for Storybook deployment
    - Updated .gitignore to exclude the new docs directory.
    - Changed the build output path for Storybook to point to docs.
    - Added a GitHub Actions workflow to automate the deployment of Storybook to GitHub Pages.

--- dist/components/Card.d.ts
+++ dist/components/Card.d.ts
@@ -1,4 +1,4 @@
-import { default as React } from 'react';
+import { default as React } from '../../node_modules/react';
 export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
     children: React.ReactNode;
     align?: 'center' | 'start';
